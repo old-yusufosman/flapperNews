8f594d07f37db504079b5c2999696623b9d175ac
Yusuf Osman <yusuf.o@gmail.com>
2016-02-10 23:20:25 +0000

    Set up routing

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,30 +1,65 @@
-var app= angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router'])
 
-app.factory('posts', [function(){
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            })
+            .state('posts', {
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
+            });
+
+        $urlRouterProvider.otherwise('home');
+    }]);
+
+app.factory('posts', [function () {
     var o = {
-        posts:[]
+        posts: []
     };
     return o;
 }]);
 
+
 app.controller('MainCtrl', [
     '$scope',
     'posts',
-    function($scope, posts){
+    function ($scope, posts) {
         $scope.posts = posts.posts;
-        $scope.incrementUpvotes = function(post){
+        $scope.incrementUpvotes = function (post) {
             post.upvotes += 1;
         }
-        $scope.addPost = function(){
-            if (!$scope.title || $scope.title ===''){
+        $scope.addPost = function () {
+            if (!$scope.title || $scope.title === '') {
                 return;
             }
+
             $scope.posts.push({
                 title: $scope.title,
+                link: $scope.link,
                 upvotes: 0,
-                link: $scope.link
+                comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                ]
             });
             $scope.title = '';
             $scope.link = '';
         }
     }]);
+
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts){
+        $scope.post = posts.posts[$stateParams.id];
+    }
+])
