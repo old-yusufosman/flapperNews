3fc163ae7b5ff7fc6af251a9d2f88e463b8fff34
Yusuf Osman <yusuf.o@gmail.com>
2016-02-11 14:02:14 +0000

    Fixed comment voting, introduced mongo

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -61,5 +61,18 @@ app.controller('PostsCtrl', [
     'posts',
     function($scope, $stateParams, posts){
         $scope.post = posts.posts[$stateParams.id];
+        $scope.incrementUpvotes = function (post) {
+            post.upvotes += 1;
+        }
+        $scope.addComment = function()
+        {
+            if($scope.body ===''){return;}
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+            $scope.body = '';
+        }
     }
 ])
