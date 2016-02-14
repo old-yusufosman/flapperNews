70fe5404cf54e101af8a59642835dd64017a2f5b
Yusuf Osman <yusuf.o@gmail.com>
2016-02-14 22:03:28 +0000

    Before auth factory

--- routes/index.js
+++ routes/index.js
@@ -35,8 +35,9 @@ router.get('/posts/:post', function(req, res, next) {
 });
 
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
     var post = new Post(req.body);
+    post.author = req.payload.username;
 
     post.save(function(err, post){
         if(err){ return next(err); }
@@ -70,7 +71,7 @@ router.param('comment', function(req, res, next, id) {
 });
 
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
     req.post.upvote(function(err, post){
         if (err) { return next(err); }
 
@@ -79,7 +80,7 @@ router.put('/posts/:post/upvote', function(req, res, next) {
 });
 
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
     req.comment.upvote(function(err, post){
         if (err) { return next(err); }
 
@@ -87,9 +88,10 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
     });
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
     var comment = new Comment(req.body);
     comment.post = req.post;
+    comment.author = req.payload.username;
 
     comment.save(function(err, comment){
         if(err){ return next(err); }
