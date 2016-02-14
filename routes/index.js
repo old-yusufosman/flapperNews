9f39a4e0d3ec210200eadf95275f70a83ca7e0d0
Yusuf Osman <yusuf.o@gmail.com>
2016-02-14 21:59:11 +0000

    Everything working so far

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var Post = mongoose.model('Post')
-var Comment = mongoose.model('Comment')
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+var passport = require('passport');
+var User = mongoose.model('User');
+var jwt = require('express-jwt');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
+
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 module.exports = router;
 
@@ -96,4 +101,38 @@ router.post('/posts/:post/comments', function(req, res, next) {
             res.json(comment);
         });
     });
+});
+
+router.post('/register', function(req, res, next){
+    if(!req.body.username || !req.body.password){
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
+
+    var user = new User();
+
+    user.username = req.body.username;
+
+    user.setPassword(req.body.password)
+
+    user.save(function (err){
+        if(err){ return next(err); }
+
+        return res.json({token: user.generateJWT()})
+    });
+});
+
+router.post('/login', function(req, res, next){
+    if(!req.body.username || !req.body.password){
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
+
+    passport.authenticate('local', function(err, user, info){
+        if(err){ return next(err); }
+
+        if(user){
+            return res.json({token: user.generateJWT()});
+        } else {
+            return res.status(401).json(info);
+        }
+    })(req, res, next);
 });
